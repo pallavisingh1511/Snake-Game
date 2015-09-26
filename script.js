d2918c56b6267ad721eeccea59df71e3e842a132
Pallavi Singh <pallavi.singh@outlook.in>
2015-09-26 17:22:33 +0530

    Basic scripting done

---
+++ script.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+    //Define variables
+    var canvas = $('#canvas')[0];
+    var ctx = canvas.getContext("2d");
+    var w = canvas.width;
+    var h = canvas.height;
+    var cw = 15;
+    var d = "right";
+    var food;
+    var score;
+    var speed = 130;
+    
+    //Snake Array
+    var snake_array;
+    
+    //Inintialize
+    function init(){
+        create_snake();
+        create_food();
+        score = 0;
+        
+        if(type game_loop != "undefined") clearInterval(game_loop);
+        game_loop = setInterval(paint, speed);
+    }
+    
+    init();
+    
+    //Create Snake
+    function create_snake(){
+        var length = 5;
+        snake_array = [];
+        for(var i = length - 1; i >=0; i--){
+            snake_array.push({x : 1, y : 0});
+        }
+    }
+    
+    //Create Food
+    function create_food(){
+        food = {
+            x:Math.round(Math.random() * ( w - cw ) / cw),
+            y:Math.round(Math.random() * ( h - cw ) / cw),
+        };
+    }
+    
+    //Paint Snake
+    function paint(){
+        //Paint the canvas
+        ctx.fillStyle = "black";
+        ctx.fillRect( 0, 0, w, h);
+        ctx.strokeStyle = "white";
+        ctx.strokeRect( 0, 0, w, h);
+        
+        var nx = snake_array[0].x;
+        var ny = snake_array[0].y;
+        
+        if(d == 'right') nx++;
+        else if (d == 'left') nx--;
+        else if (d == 'up') ny--;
+        else if (d == 'down') ny++;   
+        
+        if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array){
+                init();
+            return;
+           }
+    
+        if(nx == food.x && ny == food.y){
+            var tail = {x: nx, y :ny};
+            score++;
+            //Create food
+            create_food();
+        }    else{
+                var tail = snake_array.pop();
+                tail.x = nx; tail.y = ny;
+        }
+    
+    snake_array.unshift(tail);
+    
+    for(var i = 0; i < snake_array.length;i++){
+        var c = snake_array[i];
+        paint_cell(c.x, c.y);
+    }
+    
+    //Paint Cell
+    paint_cell( food.x, food.y)
+    
+    //Check Score
+    checkscore(score);
+    }
+});
