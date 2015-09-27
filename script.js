dc6f6c943d86596e083d937dce60850907c9f372
Pallavi Singh <pallavi.singh@outlook.in>
2015-09-28 03:52:16 +0530

    Script modified

--- script.js
+++ script.js
@@ -67,7 +67,6 @@ $(document).ready(function(){
                 $('#final_score').html(score);
                 //Show Overlay
                 $('#overlay').fadeIn(300);
-                alert(score);
                 return;
            }
     
